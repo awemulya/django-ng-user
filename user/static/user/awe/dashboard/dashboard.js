6f310d478888a485928e6344aff6e8f4527e48d7
awemulya <aweamulya@gmail.com>
2015-09-05 23:24:26 +0545

    club players modal

--- user/static/user/awe/dashboard/dashboard.js
+++ user/static/user/awe/dashboard/dashboard.js
@@ -10,6 +10,10 @@ angular.module('myApp.dashboard', ['ngRoute'])
   $routeProvider.when('/dashboard', {
     templateUrl: djstatic('user/awe/dashboard/index.html'),
     controller: 'DashboardController'
+  })
+  $routeProvider.when('/clubs', {
+    templateUrl: djstatic('user/awe/dashboard/club/club.html'),
+    controller: 'ClubController'
   });
 }])
 
@@ -88,8 +92,59 @@ angular.module('myApp.dashboard', ['ngRoute'])
     };
 })
 
+.controller('ClubPlayerModalController', function($scope, $modalInstance, players) {
+    var playerModal = $scope;
+    playerModal.players = players;
 
-.controller('MainController', ['$scope', function($scope) {
+     self.profileImg = [{
+        src: djstatic('user/vendor/dist/img/user2-160x160.jpg'),
+    }];
+
+    playerModal.ok = function() {
+        $modalInstance.close();
+    };
+
+    playerModal.cancel = function() {
+        $modalInstance.dismiss('cancel');
+    };
+})
+
+
+.controller('ClubController', ['$scope', 'Clubs', '$modal', '$timeout', function($scope, Clubs, $modal, $timeout) {
  var self = $scope;
- self.name = "maincontroller";
-}]);
+ self.clubs = Clubs.query();
+ self.profileImg = [{
+        src: djstatic('user/vendor/dist/img/user2-160x160.jpg'),
+    }];
+
+    self.openPlayers = function(players) {
+        var modalInstance = $modal.open({
+            animation: true,
+            templateUrl: djstatic('user/awe/dashboard/club/players_modal.html'),
+            controller: 'ClubPlayerModalController',
+            windowClass: 'app-modal-window',
+            resolve: {
+                players: function() {
+                    return players;
+                }
+            }
+        });
+        modalInstance.result.then(function() {
+        });
+    };
+    self.openPlayerFixtures = function(player) {
+        var modalInstance = $modal.open({
+            animation: true,
+            templateUrl: djstatic('user/awe/dashboard/fixtures_modal.html'),
+            controller: 'FixtureModalController',
+            windowClass: 'app-modal-window',
+            resolve: {
+                player: function() {
+                    return player;
+                }
+            }
+        });
+        modalInstance.result.then(function() {
+        });
+    };
+}])
--- user/static/user/awe/dashboard/services.js
+++ user/static/user/awe/dashboard/services.js
@@ -6,45 +6,9 @@ angular.module('dashboardServices', ['ngResource'])
 	});
 }])
 
-.factory('LibraryMusic', ['$resource', function($resource){
-	return $resource('app/create/libraryMusic/', {}, {
+.factory('Clubs', ['$resource', function($resource){
+	return $resource('/app/clubs/', {}, {
 	  query: {method:'GET', params:{}, isArray:true}
-	});
-}])
-
-.factory('HistoryMusic', ['$resource', function($resource){
-	return $resource('app/create/historyMusic/', {}, {
-	  query: {method:'GET', params:{}, isArray:true}
-	});
-}])
-
-.factory('HistoryImages', ['$resource', function($resource){
-	return $resource('app/create/historyImages/', {}, {
-	  query: {method:'GET', params:{}, isArray:true}
-	});
-}])
-
-.factory('Images', ['$resource', function($resource){
-	return $resource('pv/style/images.json', {}, {
-	  query: {method:'GET', params:{}, isArray:true}
-	});
-}])
-
-.factory('Create', ['$resource', function($resource){
-	return $resource('app/create/initial/', {}, {
-	  query: {method:'GET', params:{}, isArray:false}
-	});
-}])
-
-.factory('VideoPreview', ['$resource', function($resource){
-	return $resource('app/create/preview/', {}, {
-	  query: {method:'GET', params:{}, isArray:false}
-	});
-}])
-
-.factory('VideoCreate', ['$resource', function($resource){
-	return $resource('app/create/render/', {}, {
-	  query: {method:'GET', params:{}, isArray:false}
 	});
 }])
 
@@ -54,42 +18,6 @@ angular.module('dashboardServices', ['ngResource'])
 	  post: {method:'POST',  params: {}},
         update: {method:'PUT', params: {videoId: '@videoId'}},
 	});
-}])
+}]);
 
-.factory('VideoMusic', ['$resource', function($resource){
-	return $resource('app/create/:route/:musicId/', {}, {
-//	  query: {method:'GET', params:{}, isArray:true},
-	  post: {method:'POST',  params: {route:'music-detail'}},
-        update: {method:'PUT', params: {route:'music-detail', musicId: '@musicId'}},
-	});
-}])
-
-.factory('UrlMusic', ['$resource', function($resource){
-	return $resource('app/create/uploadMusic/', {}, {
-	  post: {method:'POST',  params: {}},
-        update: {method:'PUT', params: {}},
-	});
-}])
-
-.factory('UrlSlide', ['$resource', function($resource){
-	return $resource('app/create/uploadSlide/', {}, {
-	  post: {method:'POST',  params: {}},
-        update: {method:'PUT', params: {}},
-	});
-}])
-
-.factory('Credits', ['$resource', function($resource){
-	return $resource('app/create/creditsSave/', {}, {
-	  post: {method:'POST',  params: {}},
-        update: {method:'PUT', params: {}},
-	});
-}])
-
-.factory('VideoStyle', ['$resource', function($resource){
-	return $resource('app/create/:route/:styleId/', {}, {
-//	  query: {method:'GET', params:{route:'style',styleId: ''}, isArray:true},
-	  post: {method:'POST',  params: {route:'style-detail'}},
-        update: {method:'PUT', params: {route:'style-detail',styleId: '@styleId'}},
-	});
-}])
 
